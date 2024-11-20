f52dfdc60cf73e995ab9c70b29e7b0e9a30d4595
Ahmad-Zainudin-Fanani <azfann.natinons@gmail.com>
2024-11-20 15:19:02 +0700

    modifikasi

--- pertemuan13/kafe02.java
+++ pertemuan13/kafe02.java
@@ -1,7 +1,22 @@
 package pertemuan13;
 
 public class kafe02 {
-    public static void Menu() {
+    public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
+        System.out.println("Selamat datang, " + namaPelanggan + "!");
+
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+
+        // Cek kode promo
+        if (kodePromo.equals("DISKON50")) {
+            System.out.println("Selamat! Anda mendapatkan diskon 50%!");
+        } else if (kodePromo.equals("DISKON30")) {
+            System.out.println("Selamat! Anda mendapatkan diskon 30%!");
+        } else if (!kodePromo.equals("")) {
+            System.out.println("Kode promo invalid.");
+        }
+
         System.out.println("==== MENU RESTO KAFE ====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappuccino - Rp 20,000");
@@ -12,7 +27,15 @@ public class kafe02 {
         System.out.println("===========================");
         System.out.println("Silakan pilih menu yang Anda inginkan.");
     }
+
     public static void main(String[] args) {
-        Menu();
+        // Memanggil method dengan kode promo yang valid
+        Menu("Andi", true, "DISKON50");
+
+        // Memanggil method dengan kode promo yang invalid
+        Menu("Budi", false, "DISKON10");
+
+        // Memanggil method tanpa kode promo
+        Menu("Cici", true, "");
     }
 }
